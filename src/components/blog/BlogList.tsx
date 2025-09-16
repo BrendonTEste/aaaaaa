847f42a58cb3ac65368c93fa710e8ad64731be61
BrendonTEste <syneraerp@gmail.com>
2025-09-16 07:27:45 -0300

    Updated BlogList.tsx

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Calendar, User, Tag, Play, ArrowRight, X, Clock } from 'lucide-react';
